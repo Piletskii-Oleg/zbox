36a9120129893a784ea584560ff547f8841661b1
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-10-21 20:13:43 +0300

    move rabin chunker to its own file

--- src/content/chunker.rs
+++ src/content/chunker.rs
@@ -1,276 +1,34 @@
-use std::cmp::min;
+mod rabin;
+
 use std::fmt::{self, Debug};
 use std::io::{Result as IoResult, Seek, SeekFrom, Write};
-use std::ops::{Deref, DerefMut, Index, IndexMut, Range};
-
-use serde::{Deserialize, Serialize};
-
-// taken from pcompress implementation
-// https://github.com/moinakg/pcompress
-const PRIME: u64 = 153_191u64;
-const MASK: u64 = 0x00ff_ffff_ffffu64;
-const MIN_SIZE: usize = 16 * 1024; // minimal chunk size, 16k
-const AVG_SIZE: usize = 32 * 1024; // average chunk size, 32k
-const MAX_SIZE: usize = 64 * 1024; // maximum chunk size, 64k
-
-// Irreducible polynomial for Rabin modulus, from pcompress
-const FP_POLY: u64 = 0xbfe6_b8a5_bf37_8d83u64;
-
-// since we will skip MIN_SIZE when sliding window, it only
-// needs to target (AVG_SIZE - MIN_SIZE) cut length,
-// note the (AVG_SIZE - MIN_SIZE) must be 2^n
-const CUT_MASK: u64 = (AVG_SIZE - MIN_SIZE - 1) as u64;
-
-// rolling hash window constants
-const WIN_SIZE: usize = 16; // must be 2^n
-const WIN_MASK: usize = WIN_SIZE - 1;
-const WIN_SLIDE_OFFSET: usize = 64;
-const WIN_SLIDE_POS: usize = MIN_SIZE - WIN_SLIDE_OFFSET;
-
-// writer buffer length
-const WTR_BUF_LEN: usize = 8 * MAX_SIZE;
-
-/// Pre-calculated chunker parameters
-#[derive(Clone, Deserialize, Serialize)]
-pub struct ChunkerParams {
-    poly_pow: u64,     // poly power
-    out_map: Vec<u64>, // pre-computed out byte map, length is 256
-    ir: Vec<u64>,      // irreducible polynomial, length is 256
-}
-
-impl ChunkerParams {
-    pub fn new() -> Self {
-        let mut cp = ChunkerParams::default();
-
-        // calculate poly power, it is actually PRIME ^ WIN_SIZE
-        for _ in 0..WIN_SIZE {
-            cp.poly_pow = (cp.poly_pow * PRIME) & MASK;
-        }
-
-        // pre-calculate out map table and irreducible polynomial
-        // for each possible byte, copy from PCompress implementation
-        for i in 0..256 {
-            cp.out_map[i] = (i as u64 * cp.poly_pow) & MASK;
-
-            let (mut term, mut pow, mut val) = (1u64, 1u64, 1u64);
-            for _ in 0..WIN_SIZE {
-                if (term & FP_POLY) != 0 {
-                    val += (pow * i as u64) & MASK;
-                }
-                pow = (pow * PRIME) & MASK;
-                term *= 2;
-            }
-            cp.ir[i] = val;
-        }
-
-        cp
-    }
-}
-
-impl Debug for ChunkerParams {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "ChunkerParams()")
-    }
-}
-
-impl Default for ChunkerParams {
-    fn default() -> Self {
-        let mut ret = ChunkerParams {
-            poly_pow: 1,
-            out_map: vec![0u64; 256],
-            ir: vec![0u64; 256],
-        };
-        ret.out_map.shrink_to_fit();
-        ret.ir.shrink_to_fit();
-        ret
-    }
-}
-
-struct ChunkerBuf {
-    pos: usize,
-    clen: usize,
-    buf: Vec<u8>, // chunker buffer, fixed size: WTR_BUF_LEN
-}
-
-impl Index<Range<usize>> for ChunkerBuf {
-    type Output = [u8];
-
-    fn index(&self, index: Range<usize>) -> &Self::Output {
-        &self.buf[index]
-    }
-}
-
-impl Index<usize> for ChunkerBuf {
-    type Output = u8;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.buf[index]
-    }
-}
-
-impl Deref for ChunkerBuf {
-    type Target = [u8];
-
-    fn deref(&self) -> &Self::Target {
-        &self.buf
-    }
-}
-
-impl IndexMut<usize> for ChunkerBuf {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.buf[index]
-    }
-}
-
-impl IndexMut<Range<usize>> for ChunkerBuf {
-    fn index_mut(&mut self, index: Range<usize>) -> &mut Self::Output {
-        &mut self.buf[index]
-    }
-}
-
-impl DerefMut for ChunkerBuf {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.buf
-    }
-}
-
-impl ChunkerBuf {
-    fn new() -> Self {
-        let mut buf = vec![0u8; WTR_BUF_LEN];
-        buf.shrink_to_fit();
-
-        Self {
-            pos: WIN_SLIDE_POS,
-            clen: 0,
-            buf,
-        }
-    }
-
-    fn reset_position(&mut self) {
-        let left_len = self.clen - self.pos;
-        let copy_range = self.pos..self.clen;
-
-        self.buf.copy_within(copy_range, 0);
-        self.clen = left_len;
-        self.pos = 0;
-    }
-
-    fn copy_into(&mut self, buf: &[u8], in_len: usize) {
-        let copy_range = self.clen..self.clen + in_len;
-        self.buf[copy_range].copy_from_slice(&buf[..in_len]);
-        self.clen += in_len;
-    }
-
-    fn has_enough_space(&self) -> bool {
-        self.pos < self.clen
-    }
-}
+use crate::content::chunker::rabin::RabinChunker;
 
 /// Chunker
 pub struct Chunker<W: Write + Seek> {
-    dst: W,                // destination writer
-    params: ChunkerParams, // chunker parameters
-    chunk_len: usize,
-    win_idx: usize,
-    roll_hash: u64,
-    win: [u8; WIN_SIZE], // rolling hash circle window
-    buf: ChunkerBuf
+    chunker: RabinChunker<W>
 }
 
 impl<W: Write + Seek> Chunker<W> {
-    pub fn new(params: ChunkerParams, dst: W) -> Self {
+    pub fn new(dst: W) -> Self {
         Self {
-            dst,
-            params,
-            chunk_len: WIN_SLIDE_POS,
-            win_idx: 0,
-            roll_hash: 0,
-            win: [0u8; WIN_SIZE],
-            buf: ChunkerBuf::new(),
+            chunker: RabinChunker::new(dst),
         }
     }
 
-    pub fn into_inner(mut self) -> IoResult<W> {
-        self.flush()?;
-        Ok(self.dst)
+    pub fn into_inner(self) -> IoResult<W> {
+        self.chunker.into_inner()
     }
 }
 
 impl<W: Write + Seek> Write for Chunker<W> {
     // consume bytes stream, output chunks
     fn write(&mut self, buf: &[u8]) -> IoResult<usize> {
-        if buf.is_empty() {
-            return Ok(0);
-        }
-
-        // copy source data into chunker buffer
-        let in_len = min(WTR_BUF_LEN - self.buf.clen, buf.len());
-        assert!(in_len > 0);
-        self.buf.copy_into(buf, in_len);
-
-        while self.buf.has_enough_space() {
-            // get current byte and pushed out byte
-            let ch = self.buf[self.buf.pos];
-            let out = self.win[self.win_idx] as usize;
-            let pushed_out = self.params.out_map[out];
-
-            // calculate Rabin rolling hash
-            self.roll_hash = (self.roll_hash * PRIME) & MASK;
-            self.roll_hash += u64::from(ch);
-            self.roll_hash = self.roll_hash.wrapping_sub(pushed_out) & MASK;
-
-            // forward circle window
-            self.win[self.win_idx] = ch;
-            self.win_idx = (self.win_idx + 1) & WIN_MASK;
-
-            self.chunk_len += 1;
-            self.buf.pos += 1;
-
-            if self.chunk_len >= MIN_SIZE {
-                let chksum = self.roll_hash ^ self.params.ir[out];
-
-                // reached cut point, chunk can be produced now
-                if (chksum & CUT_MASK) == 0 || self.chunk_len >= MAX_SIZE {
-                    // write the chunk to destination writer,
-                    // ensure it is consumed in whole
-                    let write_range = self.buf.pos - self.chunk_len..self.buf.pos;
-                    let written = self.dst.write(&self.buf[write_range])?;
-                    assert_eq!(written, self.chunk_len);
-
-                    // not enough space in buffer, copy remaining to
-                    // the head of buffer and reset buf position
-                    if self.buf.pos + MAX_SIZE >= WTR_BUF_LEN {
-                        self.buf.reset_position();
-                    }
-
-                    // jump to next start sliding position
-                    self.buf.pos += WIN_SLIDE_POS;
-                    self.chunk_len = WIN_SLIDE_POS;
-                }
-            }
-        }
-
-        Ok(in_len)
+        self.chunker.write(buf)
     }
 
     fn flush(&mut self) -> IoResult<()> {
-        // flush remaining data to destination
-        let p = self.buf.pos - self.chunk_len;
-        if p < self.buf.clen {
-            self.chunk_len = self.buf.clen - p;
-            let write_range = p..p + self.chunk_len;
-            let _ = self.dst.write(&self.buf.buf[write_range])?;
-        }
-
-        // reset chunker
-        self.buf.pos = WIN_SLIDE_POS;
-        self.buf.clen = 0;
-        self.chunk_len = WIN_SLIDE_POS;
-        self.win_idx = 0;
-        self.roll_hash = 0;
-        self.win = [0u8; WIN_SIZE];
-
-        self.dst.flush()
+        self.chunker.flush()
     }
 }
 
@@ -282,7 +40,7 @@ impl<W: Write + Seek> Debug for Chunker<W> {
 
 impl<W: Write + Seek> Seek for Chunker<W> {
     fn seek(&mut self, pos: SeekFrom) -> IoResult<u64> {
-        self.dst.seek(pos)
+        self.chunker.seek(pos)
     }
 }
 
@@ -356,7 +114,6 @@ mod tests {
 
         // perpare test data
         const DATA_LEN: usize = 765 * 1024;
-        let params = ChunkerParams::new();
         let mut data = vec![0u8; DATA_LEN];
         Crypto::random_buf(&mut data);
         let mut cur = Cursor::new(data);
@@ -366,7 +123,7 @@ mod tests {
         };
 
         // test chunker
-        let mut ckr = Chunker::new(params, sinker);
+        let mut ckr = Chunker::new(sinker);
         let result = copy(&mut cur, &mut ckr);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), DATA_LEN as u64);
@@ -379,7 +136,6 @@ mod tests {
 
         // perpare test data
         const DATA_LEN: usize = 10 * 1024 * 1024;
-        let params = ChunkerParams::new();
         let mut data = vec![0u8; DATA_LEN];
         let seed = RandomSeed::from(&[0u8; RANDOM_SEED_SIZE]);
         Crypto::random_buf_deterministic(&mut data, &seed);
@@ -387,7 +143,7 @@ mod tests {
         let sinker = VoidSinker {};
 
         // test chunker performance
-        let mut ckr = Chunker::new(params, sinker);
+        let mut ckr = Chunker::new(sinker);
         let now = Instant::now();
         copy(&mut cur, &mut ckr).unwrap();
         ckr.flush().unwrap();
